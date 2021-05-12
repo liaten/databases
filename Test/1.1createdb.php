217d11d023a3944a7d0fb4d9745e3b25843564be
liaten <35489460+liaten@users.noreply.github.com>
2021-05-12 14:49:13 +0300

    Add files via upload

---
+++ Test/1.1createdb.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>MySQL Test</title>
+</head>
+<body>
+<?php
+   $dbhost = 'localhost';
+   $dbuser = 'root';
+   $dbpass = '';
+   $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+   //$dbname = 'PESTEREVVLAD';
+   $dbname = $_POST['username'];
+   if(! $conn )
+      {die('Could not connect');}
+   echo 'MySQL connected successfully<br>';
+   $sql = 'CREATE DATABASE '.$dbname; //создание базы данных PESTEREVVLAD
+   $retval = mysqli_query( $conn, $sql );
+   if($retval)
+      {echo 'Database '.$dbname.' created successfully<br>';}
+   else
+      {echo 'Database '.$dbname.' already exists<br>';}
+    mysqli_close($conn);
+?>
+</body>
+</html>
